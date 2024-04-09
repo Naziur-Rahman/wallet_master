37da926776f77bafa614612902a734227db889a0
Naziur-Rahman <naziurr85@gmail.com>
2024-04-09 23:17:38 +0600

    updat

--- assets/js/tailwind.js
+++ assets/js/tailwind.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./**/*.{html,js}"],
+  safelist: [
+    '!duration-[0ms]',
+    '!delay-[0ms]',
+    'html.js :where([class*="taos:"]:not(.taos-init))'
+  ],
+  content: {
+    relative: true,
+    transform: (content) => content.replace(/taos:/g, ''),
+    files: ['*.{html,js}'],
+  },
     theme: {
       extend: {},
     },
-    plugins: [],
+    plugins: [
+      require('taos/plugin')
+    ],
   }
